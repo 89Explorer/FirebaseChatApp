59f4cb07438f54fbe9262991c372bdc71ec9f5ec
89Explorer <leral3@naver.com>
2025-01-14 03:41:39 +0900

    Initial Commit

---
+++ FirebaseChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseChatApp
+//
+//  Created by 권정근 on 1/14/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FirebaseChatAppTests/FirebaseChatAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseChatAppTests.swift
+//  FirebaseChatAppTests
+//
+//  Created by 권정근 on 1/14/25.
+//
+
+import Testing
+@testable import FirebaseChatApp
+
+struct FirebaseChatAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
